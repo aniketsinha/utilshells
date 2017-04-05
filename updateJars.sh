61ff9b9529a9ccbc7e729a37c6af6f3846e4219e
Aniket Sinha <aniketsinha@users.noreply.github.com>
2017-04-05 18:52:16 +0530

    conditional jars update by params

--- updateJars.sh
+++ updateJars.sh
@@ -1,9 +1,24 @@
 #!/bin/sh
 #Update jars with class files
 #Here class files are present in $WORKSPACE/output which neeed to be updated in $BUILD_LIB_DIR/JarName.jar file
+#EDIT: If only Conditional.jar needs to be updated, pass any param: eg. `sh updateJars.sh a`
 
 BUILD_LIB_DIR=/path/to/lib
 WORKSPACE=/Users/aniket/Documents/workspace/project_name
 
-cd $WORKSPACE/output
-jar -uvf $BUILD_LIB_DIR/JarName.jar com/
+if [ $# != 0 ]
+then
+  echo Params received, updating only conditional jar: Conditional.jar
+
+  cd $WORKSPACE/conditional
+  jar -uvf $BUILD_LIB_DIR/Conditional.jar com/
+
+else
+ echo no params passed, updating all jars
+ 
+ cd $WORKSPACE/output
+ jar -uvf $BUILD_LIB_DIR/JarName.jar com/
+ 
+ cd $WORKSPACE/conditional
+ jar -uvf $BUILD_LIB_DIR/Conditional.jar com/
+fi
